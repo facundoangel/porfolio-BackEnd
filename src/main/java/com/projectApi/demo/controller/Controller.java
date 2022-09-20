3967818415ace251d6fb0836dbbbb78afd78cb46
facundoangel <facundonicolasangel@gmail.com>
2022-09-19 23:45:08 -0300

    configuracion nueva cors

--- src/main/java/com/projectApi/demo/controller/Controller.java
+++ src/main/java/com/projectApi/demo/controller/Controller.java
@@ -43,7 +43,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author facun
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:4200", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
+@CrossOrigin(origins = "https://porftolio-angular.web.app", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class Controller {
     
     
